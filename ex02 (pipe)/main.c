ba3f19c7c1ca5d1607e2d5336020907890c83263
Musa Kocabas <mkocabas@k2m17s05.42kocaeli.com.tr>
2023-03-17 01:58:11 +0300

    ex02 add

---
+++ ex02 (pipe)/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+	// kanalın giriş ve çıkış yönlerini temsilen
+	int     fd[2], nbytes;
+	
+	// mesajın yönünü ifade etmek için çekirdek tarafından döndürülen değer
+	int   childpid;
+	
+	// gönderilecek olan mesaj
+	char    msg[] = "musa kocabaş\n";
+	
+	// maksimum mesaj boyutu
+	char    readbuffer[40];
+	
+	// iletişim kanalını oluşturan sistem çağrısı
+	pipe(fd);
+	
+	// hata kontrolü
+	if((childpid = fork()) == -1) {
+		perror("fork işlemi başarısız!");
+		exit(1);
+	}
+	
+	if(childpid == 0) {
+		// child procress
+		// kanalın giriş tarafını kapat
+		close(fd[0]);
+		
+		// çıkış tarafına mesajı yazdır
+		write(fd[1], msg, (strlen(msg)+1));
+		printf("Kanaldan gönderilen mesaj: %s", msg);
+		exit(0);
+	} else {
+		// kanalın çıkış tarafını kapat
+		close(fd[1]);
+		
+		// kanaldan gelen mesajı oku
+		nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+		printf("Kanaldan gelen mesaj: %s", readbuffer);
+	}
+	
+	return(0);
+}
