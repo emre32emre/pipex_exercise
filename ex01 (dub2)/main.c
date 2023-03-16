30a32c618af0543fd66d4b66caaecec2572fa273
Musa Kocabas <mkocabas@k2m17s05.42kocaeli.com.tr>
2023-03-16 18:36:49 +0300

    update dub2

--- ex01 (dub2)/main.c
+++ ex01 (dub2)/main.c
@@ -12,13 +12,20 @@ int main()
 	// , newfd'yi kapatır ve oldfd'yi newfd'ye atar
 	
 	int fd = open("test.txt",O_CREAT | O_RDWR ,777);
+	int orginal_stdout = dup(STDOUT_FILENO);
 
 	chmod("test.txt",S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 	
 	printf("Bu çıktı stdout a gidicek fd:%d \n",fd);
+	fflush(stdout);
 
 	dup2(fd,STDOUT_FILENO);
 
-	printf("bu çıktı dosyaya yönlendirilecek\n");
+	printf("bu çıktı dosyaya yönlendirilecek \n");
+
+
+	dup2(orginal_stdout, STDOUT_FILENO);
+
+	printf("çıktı tekrar STDOUT yönlendirildi\n");
 
 }
